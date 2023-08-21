bee3a68085763d635be5f7d3d58eb10959a2cfe3
kyle-233 <kyle07142113@163.com>
2023-08-21 10:38:15 +0800

    feat:完成首页

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer } from './sections'
+import { Nav } from './components'
 
 const App = () => {
 
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-l wide:padding-r padding-b">Hero</section>
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b"><Hero /></section>
       <section className="padding">
-        PopularProducts
+        <PopularProducts />
       </section>
       <section className="padding">
-        SuperQuality
+        <SuperQuality />
       </section>
       <section className="padding-x py-10">
-        Services
+        <Services />
       </section>
       <section className="padding">
-        SpecialOffer
+        <SpecialOffer />
       </section>
       <section className="bg-pale-blue padding">
-        CustomerReviews
+        <CustomerReviews />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        Subscribe
+        <Subscribe />
       </section>
       <section className="bg-black padding-x padding-t pb-8">
-        Footer
+        <Footer />
       </section>
     </main>
   )
